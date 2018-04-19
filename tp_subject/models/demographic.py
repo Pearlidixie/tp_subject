ebf8e52e409e15c45ead524d535d7cdb98f9415a
pmalapeng <pmalapeng@gmail.com>
2018-04-19 09:57:22 +0200

    created the Demographic model with its fields

---
+++ tp_subject/models/demographic.py
@@ -0,0 +1,24 @@
+from django.db import models
+from tp_subject.choices import MARITAL_STATUS, LIVES_WITH
+
+
+class Demographic(models.Model):
+    #print("models")
+    marital_status = models.CharField(
+        max_length=10,
+        choices=MARITAL_STATUS)
+
+    number_wives = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="If subject is a woman, how many wives does the husband have (traditional wives and subject included)",)
+
+    number_wives_men = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="If subject is a man, how many wives does he have (traditional marriage included)?",)
+
+    lives_with = models.CharField(
+        max_length=10,
+        verbose_name="Who do you currently live with?",
+        choices=LIVES_WITH)
