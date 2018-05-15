ec5a992822e913e667002e9479114e7fe0ac45ea
pmalapeng <pmalapeng@gmail.com>
2018-05-15 14:54:00 +0200

    created the subject consent admin

---
+++ tp_subject/admin/subject_consent_admin.py
@@ -0,0 +1,77 @@
+from django.contrib import admin
+from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
+from edc_model_admin import (
+    ModelAdminFormAutoNumberMixin, ModelAdminInstitutionMixin,
+    audit_fieldset_tuple, ModelAdminNextUrlRedirectMixin,
+     ModelAdminReplaceLabelTextMixin)
+from simple_history.admin import SimpleHistoryAdmin
+
+from tp_subject.forms.subject_consent_form import SubjectConsentForm
+from tp_subject.models.subject_consent import SubjectConsent
+
+
+class ModelAdminMixin(ModelAdminNextUrlRedirectMixin, ModelAdminFormAutoNumberMixin,
+                      ModelAdminRevisionMixin, ModelAdminReplaceLabelTextMixin,
+                      ModelAdminInstitutionMixin):
+
+    list_per_page = 10
+    empty_value_display = '-'
+
+
+class SubjectConsentAdmin(ModelAdminMixin, SimpleHistoryAdmin,
+                          admin.ModelAdmin):
+
+    form = SubjectConsentForm
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'screening_identifier',
+                'subject_identifier',
+                'first_name',
+                'last_name',
+                'initials',
+                'language',
+                'is_literate',
+                'witness_name',
+                'consent_datetime',
+                'dob',
+                'guardian_name',
+                'is_dob_estimated',
+                'identity',
+                'identity_type',
+                'confirm_identity',
+                'is_incarcerated')}),
+        ('Sample collection and storage', {
+            'fields': (
+                'may_store_samples',
+                'may_store_genetic_samples')}),
+        ('Review Questions', {
+            'fields': (
+                'consent_reviewed',
+                'study_questions',
+                'assessment_score',
+                'consent_signature',
+                'consent_copy'),
+            'description': 'The following questions are directed to the interviewer.'}),
+        audit_fieldset_tuple)
+
+    search_fields = ('subject_identifier', 'screening_identifier', 'identity')
+
+    radio_fields = {
+        "assessment_score": admin.VERTICAL,
+        "consent_copy": admin.VERTICAL,
+        "consent_reviewed": admin.VERTICAL,
+        "consent_signature": admin.VERTICAL,
+        "gender": admin.VERTICAL,
+        "is_dob_estimated": admin.VERTICAL,
+        "is_incarcerated": admin.VERTICAL,
+        "is_literate": admin.VERTICAL,
+        "language": admin.VERTICAL,
+        "may_store_genetic_samples": admin.VERTICAL,
+        "may_store_samples": admin.VERTICAL,
+        "study_questions": admin.VERTICAL,
+    }
+
+
+admin.site.register(SubjectConsent, SubjectConsentAdmin)
