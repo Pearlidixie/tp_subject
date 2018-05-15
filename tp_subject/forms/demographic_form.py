f7db26bb040073fe1a1cfafb81018930acd160f6
pmalapeng <pmalapeng@gmail.com>
2018-05-15 15:11:52 +0200

    demographic form

--- tp_subject/forms/demographic_form.py
+++ tp_subject/forms/demographic_form.py
@@ -1,8 +1,12 @@
 from tp_subject.models.demographic import Demographic
+#from tp_subject_form_validators.form_validators.demographic import Demo
 from django import forms
 
 
 class DemographicForm(forms.ModelForm):
+
+    #form_validator_cls = DemographicFormValidator
+
     class Meta:
         model = Demographic
         fields = '__all__'
