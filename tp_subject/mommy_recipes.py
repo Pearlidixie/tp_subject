bea3681fe9d47cbab43426c0393870d63c7e6303
pmalapeng <pmalapeng@gmail.com>
2018-05-15 14:51:24 +0200

    added recipe for demographic model

---
+++ tp_subject/mommy_recipes.py
@@ -0,0 +1,17 @@
+from django.contrib.sites.models import Site
+from edc_base.utils import get_utcnow
+from .constants import MARRIED
+from faker import Faker
+from model_mommy.recipe import Recipe
+
+from .models import Demographic
+from tp_subject.constants import NOT_APPLICABLE
+
+fake = Faker()
+
+demographic = Recipe(
+    Demographic,
+    marital_status=MARRIED,
+    number_wives=1,
+    number_wives_men=NOT_APPLICABLE,
+    lives_with=40)
