e17519a825ad0967116c8183879c24e96e6a327d
pmalapeng <pmalapeng@gmail.com>
2018-05-15 14:58:20 +0200

    modified admin for community engagements

--- tp_subject/admin/community_engagement_admin.py
+++ tp_subject/admin/community_engagement_admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from tp_subject.models.community_engagement import CommunityEngagement
-#from tp_screening.admin_site import tp_screening_admin
 
 
-#@admin.register(SubjectScreening, site=tp_screening_admin)
 class CommunityEngagementAdmin(admin.ModelAdmin):
 
     radio_fields = {
