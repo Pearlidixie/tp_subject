28a9a1fc069eaf894a787cc37d1e77a3ec1c7e05
pmalapeng <pmalapeng@gmail.com>
2018-05-17 12:44:25 +0200

    added recipe for subject consent

--- tp_subject/mommy_recipes.py
+++ tp_subject/mommy_recipes.py
@@ -2,12 +2,36 @@ from django.contrib.sites.models import Site
 from edc_base.utils import get_utcnow
 from .constants import MARRIED
 from faker import Faker
-from model_mommy.recipe import Recipe
-
+from model_mommy.recipe import Recipe, seq
 from .models import Demographic
-from tp_subject.constants import NOT_APPLICABLE
+from tp_subject.models.subject_consent import SubjectConsent
+from tp_subject.constants import YES, NO
+from dateutil.relativedelta import relativedelta
 
 fake = Faker()
+
+subjectconsent = Recipe(
+    SubjectConsent,
+    assessment_score=YES,
+    confirm_identity=seq('12315678'),
+    consent_copy=YES,
+    consent_datetime=get_utcnow(),
+    consent_reviewed=YES,
+    dob=get_utcnow() - relativedelta(years=25),
+    first_name=fake.first_name,
+    gender='M',
+    identity=seq('12315678'),
+    identity_type='country_id',
+    initials='XX',
+    is_dob_estimated='-',
+    is_incarcerated=NO,
+    is_literate=YES,
+    last_name=fake.last_name,
+    screening_identifier=None,
+    study_questions=YES,
+    site=Site.objects.get_current(),
+    subject_identifier=None)
+
 
 demographic = Recipe(
     Demographic,
