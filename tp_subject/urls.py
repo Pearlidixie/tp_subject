e169c2e856cefda728d5b8eb58b295a8b44699d6
pmalapeng <pmalapeng@gmail.com>
2018-06-05 10:10:10 +0200

    update urls

--- tp_subject/urls.py
+++ tp_subject/urls.py
@@ -1,21 +1,11 @@
-"""tp_subject URL Configuration
+from django.urls.conf import path
+from django.views.generic.base import RedirectView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+from .admin_site import tp_subject_admin
+
+app_name = 'tp_subject'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', tp_subject_admin.urls),
+    path('', RedirectView.as_view(url='admin/'), name='home_url'),
 ]
